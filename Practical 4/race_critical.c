f0689044b666a020bb3ad3e9dad394c849f6f595
U-MSI\doher <dohertycw@gmail.com>
2024-03-13 18:27:16 +0000

    Practical 4 complete

--- Practical 4/race_critical.c
+++ Practical 4/race_critical.c
@@ -11,7 +11,7 @@ int main(void){
   printf("Please enter the size of the array\n");
   scanf("%d",&n);
   if (n<2 || n>1000) {
-    printf(" Enter a positive number in range 2<n<10001\n");
+    printf(" Enter a positive number in range 2<n<1001\n");
     exit(1);
   }
 
@@ -24,7 +24,7 @@ int main(void){
   a[0] = 0;
 
   /* Start of parallel region */
-#pragma omp parallel private(i,j,tid), shared(nthreads,n,a)
+#pragma omp  parallel num_threads(5) private(i,j,tid), shared(nthreads,n,a)
 {
   tid=omp_get_thread_num();
   nthreads=omp_get_num_threads();
@@ -34,6 +34,9 @@ int main(void){
   a[0] = a[0] + rand()%11;
 
   /* Set all elements per thread equal */
+  /*Here we ensure that each thread executes in sequence, meaning the array has the same elements*/
+  #pragma omp barrier
+  #pragma omp critical
   for (j=1; j<n; j++) a[j] = a[0];
     printf("Hello from thread %d out of %d my a is: %d\n", tid,nthreads,a[0]);
 } /* end parallel region */
